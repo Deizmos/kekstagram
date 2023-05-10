f1b52a18959ea59b24cbfb18d874df0d95e0ef99
Mikhail Filippenko <deizmos@icloud.com>
2023-05-10 13:22:47 +0400

    Fixed remark

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,20 @@
-function checkStringLength(string, maxLength) {
-  return string.length <= maxLength;
-}
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-function isPalindrome(string) {
-  const splitString = string.toLowerCase().split(' ').join('');
-  return string.toLowerCase().split(' ').join('') === splitString.split('').reverse().join('');
-}
+const isPalindrome = (string) => {
+  const splitString = string.toLowerCase().replaceAll(' ', '');
+  return string.toLowerCase().replaceAll(' ', '') === [...splitString].reverse().join('');
+};
+const isExtractNumber = (el) => typeof el === 'string' ? parseInt(el.replace(/[^0-9]/g, ''), 10) : +String(el).replace(/[^0-9]/g, '');
 
-function isExtractNumber(el) {
-  return typeof el === 'string' ? parseInt(el.replace(/[^0-9]/g, ''), 10) : +String(el).replace(/[^0-9]/g, '');
-}
-
-function isReductionLength(str, minLength, extraString) {
-  let string = str;
+const isReductionLength = (str, minLength, extraString) => {
   const arrStr = str.split('');
   const arrExtraString = extraString.split('');
   while (arrStr.length < minLength){
     const removalLength = minLength - arrStr.length;
     arrStr.unshift(...arrExtraString.slice(0,removalLength));
   }
-  string = arrStr.join('');
-  return string;
-}
+  return arrStr.join('');
+};
 
 export {
   checkStringLength,
